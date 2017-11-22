b3ca1753d3077f05a02411b712c35d7f3bf2a3cc
zachard <arieszpzeng@yeah.net>
2017-11-22 16:08:10 +0800

    finish: Spring 整合Mybatis示例
    可参考内容: (1) 数据库配置文件密码加密
                (2) Spring整合mybatis配置

---
+++ hello-spring/src/main/java/com/zachard/spring/hello/controller/DepartmentsController.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2015-2017 zachard, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.zachard.spring.hello.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zachard.spring.hello.model.Departments;
+import com.zachard.spring.hello.service.DepartmentsService;
+
+/**
+ * 部门处理请求Controller
+ * <pre>
+ * </pre>
+ *
+ * @author zachard
+ * @version 1.0.0
+ */
+@RestController
+@RequestMapping("/department")
+public class DepartmentsController {
+
+	@Resource
+	private DepartmentsService departmentsService;
+	
+	/**
+	 * 查询部门信息请求接口
+	 * 
+	 * @param deptNo    部门编号
+	 * @return          部门信息
+	 */
+	@RequestMapping(value = "/{deptNo}", method = {RequestMethod.GET})
+	public Departments query(@PathVariable("deptNo") String deptNo) {
+		return departmentsService.queryByNo(deptNo);
+	}
+	
+}
