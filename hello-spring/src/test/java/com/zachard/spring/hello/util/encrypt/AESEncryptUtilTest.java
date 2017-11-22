73812c49279a2011923e20dac7260429f63406d7
zachard <arieszpzeng@yeah.net>
2017-11-22 23:46:00 +0800

    finish: java中AES高级标准对称算法加密与解密示例
    可参考内容:

--- hello-spring/src/test/java/com/zachard/spring/hello/util/encrypt/AESEncryptUtilTest.java
+++ hello-spring/src/test/java/com/zachard/spring/hello/util/encrypt/AESEncryptUtilTest.java
@@ -30,16 +30,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * <code>DES3</code>算法加密工具测试类
+ * <code>AES</code>高级标准对称加密算法测试类
  * <pre>
  * </pre>
  *
  * @author zachard
  * @version 1.0.0
  */
-public class DES3EncryptUtilTest {
+public class AESEncryptUtilTest {
 	
-	private static DES3EncryptUtil des3EncryptUtil;
+private static AESEncryptUtil aesEncryptUtil;
 	
 	/**
 	 * 加密后的密文
@@ -54,7 +54,7 @@ public class DES3EncryptUtilTest {
 	 */
 	@BeforeClass
 	public static void setBeforeClass() throws NoSuchAlgorithmException, NoSuchPaddingException {
-		des3EncryptUtil = new DES3EncryptUtil();
+		aesEncryptUtil = new AESEncryptUtil();
 	}
 	
 	/**
@@ -62,24 +62,16 @@ public class DES3EncryptUtilTest {
 	 * 
 	 * <pre>
 	 *     放在一个方法里面执行,保证加密与解密的同步执行(先加密后解密)
-	 *     
-	 *     注: (1) 因为{@link DESEncryptUtil}与{@link DES3EncryptUtil}都是继承自
-	 *             {@link AbstractSymmetricEncryptUtil},所以测试在加密之后实例化另外
-	 *             一个子类型的对象,验证{@link SecretKey}是否会被覆盖
-	 *             结论: 不会,因为是实例化对象,所以每一个实例都会有一份成员变量拷贝,并且互不影响
 	 * </pre>
 	 * 
 	 * @throws BadPaddingException 
 	 * @throws IllegalBlockSizeException 
 	 * @throws InvalidKeyException 
 	 * @throws IOException 
-	 * @throws NoSuchPaddingException 
-	 * @throws NoSuchAlgorithmException 
 	 */
 	@Test
-	public void des3Test() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, NoSuchAlgorithmException, NoSuchPaddingException {
+	public void aesTest() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
 		encryptTest();
-		new DESEncryptUtil();
 		decryptTest();
 	}
 	
@@ -93,7 +85,7 @@ public class DES3EncryptUtilTest {
 	 */
 	private void encryptTest() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
 		String data = "zpzeng";
-		cipherText = des3EncryptUtil.encrypt(data);
+		cipherText = aesEncryptUtil.encrypt(data);
 		System.err.println("加密后的密文为: " + cipherText);
 	}
 	
@@ -106,7 +98,7 @@ public class DES3EncryptUtilTest {
 	 * @throws IOException
 	 */
 	private void decryptTest() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
-		System.err.println("解密后的原文为: " + des3EncryptUtil.decrypt(cipherText));
+		System.err.println("解密后的原文为: " + aesEncryptUtil.decrypt(cipherText));
 	}
 
 }
